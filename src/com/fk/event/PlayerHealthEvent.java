e2ec081f920d1fc7839a0af0c196abe80da74328
clipsy1 <clipsy1@gmail.com>
2018-09-04 12:48:27 +0200

    Version 0.4.2 - Day 1

--- src/com/fk/event/PlayerHealthEvent.java
+++ src/com/fk/event/PlayerHealthEvent.java
@@ -1,8 +1,10 @@
 package com.fk.event;
 
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 
 import com.fk.main.Main;
@@ -10,10 +12,17 @@ import com.fk.main.Main;
 public class PlayerHealthEvent implements Listener{
 	@EventHandler
 	public void PlayerDamageEvent(EntityDamageEvent e){
-		if(!Main.gameStatus && e.getEntityType().equals(EntityType.PLAYER)){
+		if((!Main.gameStatus || Main.day == 0) && e.getEntityType().equals(EntityType.PLAYER)){
 			e.setCancelled(true);
-		} else if(Main.gameStatus && e.getEntityType().equals(EntityType.PLAYER) && Main.day == 3) {
-			
+		}
+	}
+	
+	@EventHandler
+	public void PlayerDamageByPlayerEvent(EntityDamageByEntityEvent e){
+		if(Main.day < 3 && e.getEntityType().equals(EntityType.PLAYER)){
+			if(e.getDamager() instanceof Player){
+				e.setCancelled(true);
+			}
 		}
 	}
 
